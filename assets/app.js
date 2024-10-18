f6c088b8d811546d5833335fc8c290141875fd03
mikhawa <michaeljpitz@gmail.com>
2024-10-18 15:09:57 +0200

    temlate

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,14 @@
 import './bootstrap.js';
 import './js/jquery-3.3.1.min.js';
 import './js/bootstrap.min.js';
+import './js/player.js';
+import './js/jquery.nice-select.min.js';
+import './js/mixitup.min.js';
+import './js/jquery.slicknav.js';
+import './js/owl.carousel.min.js';
+import './js/main.js';
+
+
 /*
  * Welcome to your app's main JavaScript file!
  *
